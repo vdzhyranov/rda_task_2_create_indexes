240e5d3b8415ea092a05713c7b2589e3a3764a8a
dzhyranov <v.dzhyranov@gmail.com>
2024-05-02 09:41:50 +0300

    fix table name

--- task.sql
+++ task.sql
@@ -1,6 +1,6 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
 
-CREATE INDEX Name ON Product (Name);
+CREATE INDEX Name ON Products (Name);
 
 CREATE INDEX Email ON Customers (Email);
